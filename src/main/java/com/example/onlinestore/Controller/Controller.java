b372fdca7458e540ccaac0459fae91fa1f3d77d2
DonBahall <dimachar2004@gmail.com>
2023-06-26 19:20:18 +0200

    init

--- src/main/java/com/example/onlinestore/Controller/Controller.java
+++ src/main/java/com/example/onlinestore/Controller/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping
     public String index(){
-        return "Hello world";
+        return "Hello";
     }
 }
